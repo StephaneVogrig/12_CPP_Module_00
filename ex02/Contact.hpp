5fa06cc7838e6250304c63075d902d95c860c17f
Stephane Vogrig <svogrig@3B3.42angouleme.fr>
2024-10-29 04:34:26 +0100

    add begining ex02

---
+++ ex02/Contact.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 02:03:27 by svogrig           #+#    #+#             */
+/*   Updated: 2024/10/29 04:16:12 by svogrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class Contact
+{
+	public:
+		Contact();
+		Contact(std::string	first_name,
+				std::string	last_name,
+				std::string	nickname,
+				std::string	phone_number,
+				std::string	darkest_secret);
+
+		~Contact();
+
+		void		set_first_name(std::string first_name);
+		void		set_last_name(std::string last_name);
+		void		set_nickname(std::string nickname);
+		void		set_phone_number(std::string phone_number);
+		void		set_darkest_secret(std::string darkest_secret);
+
+		std::string	get_first_name();
+		std::string	get_last_name();
+		std::string	get_nickname();
+		std::string	get_phone_number();
+		std::string	get_darkest_secret();
+
+	private:
+		std::string	m_first_name;
+		std::string	m_last_name;
+		std::string	m_nickname;
+		std::string	m_phone_number;
+		std::string	m_darkest_secret;
+		
+};
+
+#endif
