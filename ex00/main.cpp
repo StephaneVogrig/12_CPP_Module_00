34a65967d7b49827738ac69312d2e886952c287e
Stephane Vogrig <svogrig@3B3.42angouleme.fr>
2024-10-29 01:03:29 +0100

    ex00 fini

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,23 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 16:56:00 by svogrig           #+#    #+#             */
-/*   Updated: 2024/10/28 18:27:12 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/10/29 00:59:54 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 
-int	main(){
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (--argc)
+	{
+		std::string str(*++argv);
+		for (std::string::iterator it = str.begin(); it != str.end(); it++)
+			*it = std::toupper(*it);
+		std::cout << str;
+	}
+	std::cout << std::endl;
+	return (0);
 }
